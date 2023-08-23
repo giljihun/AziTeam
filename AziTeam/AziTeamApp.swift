9ac43783b08435ce8ef63b99d0050e82931a3b12
giljihun <accc45@outlook.com>
2023-08-23 18:34:58 +0900

    Build: Firebase 연동(#1)
    프로젝트를 만들고 Firebase 앱 연동을 했다.
     - Package 추가
     - AziTeamApp.swift

--- AziTeam/AziTeamApp.swift
+++ AziTeam/AziTeamApp.swift
@@ -6,9 +6,22 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct AziTeamApp: App {
+    // Firebase 설정을 위한 앱 위임 등록
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
