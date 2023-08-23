f41431b825f8331cb9ad916fb186c1c2c1e77c68
giljihun <accc45@outlook.com>
2023-08-23 15:16:34 +0900

    Initial Commit

---
+++ AziTeam/AziTeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  AziTeamApp.swift
+//  AziTeam
+//
+//  Created by mobicom on 2023/08/23.
+//
+
+import SwiftUI
+
+@main
+struct AziTeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
